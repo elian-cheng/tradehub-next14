6297140f1f4fdbea776a46f68607d4d8c8a113b5
Olga Chernega <eliang.cheng@gmail.com>
2024-01-21 14:01:39 +0200

    feat: add invoices page and CRUD functionality

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -7,10 +7,11 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
+import { createInvoice } from '@/app/lib/actions';
 
 export default function Form({ sellers }: { sellers: SellerField[] }) {
   return (
-    <form>
+    <form action={createInvoice}>
       <div className="rounded-md bg-neutral-700 p-4 md:p-6">
         <div className="mb-4">
           <label
@@ -25,6 +26,7 @@ export default function Form({ sellers }: { sellers: SellerField[] }) {
               name="sellerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue=""
+              required
             >
               <option value="" disabled>
                 Select a seller
@@ -54,6 +56,7 @@ export default function Form({ sellers }: { sellers: SellerField[] }) {
                 type="number"
                 step="0.01"
                 placeholder="Enter USD amount"
+                required
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
@@ -73,6 +76,7 @@ export default function Form({ sellers }: { sellers: SellerField[] }) {
                   name="status"
                   type="radio"
                   value="awaiting"
+                  required
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
                 />
                 <label
@@ -88,6 +92,7 @@ export default function Form({ sellers }: { sellers: SellerField[] }) {
                   name="status"
                   type="radio"
                   value="fulfilled"
+                  required
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
                 />
                 <label
